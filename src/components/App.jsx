f8b904bfceda93759eae5c036d914e9d281a53d4
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-19 17:35:29 +0300

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import Counter from './Feedback/Feedback';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-     📔 goit-react-hw-02-feedback
-    </div>
+    <>
+     <Counter step={1} />
+    </>
   );
 };
