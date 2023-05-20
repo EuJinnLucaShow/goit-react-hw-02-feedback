033728c42ae85df2c66e9c7657ecb7eb493193b5
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-20 16:52:18 +0300

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -4,19 +4,23 @@ function Counter() {
   const [state, setState] = useState({
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
+    total: 0,
+    percentage: 0
   });
 
   const handleFeedback = (type) => {
     setState((prevState) => ({
       ...prevState,
-      [type]: prevState[type] + 1
+      [type]: prevState[type] + 1,
+      total: prevState.total + 1,
+      percentage: Math.round((prevState.good / prevState.total) * 100)
     }));
   };
 
   return (
     <div>
-      <h1>Feedback Counter</h1>
+      <h1>Please leave feedback</h1>
       <button onClick={() => handleFeedback('good')}>Good</button>
       <button onClick={() => handleFeedback('neutral')}>Neutral</button>
       <button onClick={() => handleFeedback('bad')}>Bad</button>
@@ -25,6 +29,8 @@ function Counter() {
         <p>Good: {state.good}</p>
         <p>Neutral: {state.neutral}</p>
         <p>Bad: {state.bad}</p>
+        <p>Total: {state.total}</p>
+        <p>Percentage: {state.percentage}%</p>
       </div>
     </div>
   );
