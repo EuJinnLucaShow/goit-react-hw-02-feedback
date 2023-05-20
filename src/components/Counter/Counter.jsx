4f20c5234d8a331eb82c06380d31df49daeee629
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-20 15:43:24 +0300

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,39 +1,33 @@
-import React, { Component } from "react";
-import { Div, Span, Button } from './Counter.styled'
+import React, { useState } from 'react';
 
+function Counter() {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
 
-class Counter extends Component {
-  static defaultProps = {
-    step: 1,
-    initialValue: 0,
+  const handleFeedback = (type) => {
+    setState((prevState) => ({
+      ...prevState,
+      [type]: prevState[type] + 1
+    }));
   };
 
-constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
-    this.handleDecrement = this.handleDecrement.bind(this);
-  }
-
-handleIncrement = evt => {
-    console.log("Increment button was clicked!", evt); // працює
-    
-  }
-
-  handleDecrement = evt => {
-    console.log("Decrement button was clicked!", evt); // працює
-    
-  }
-
-  render() {
-    const { step } = this.props;
-
-    return (
-      <Div>
-        <Span>0</Span>
-        <Button type="button" onClick={this.handleIncrement} >Increment by {step}</Button>
-        <Button type="button" onClick={this.handleDecrement} >Decrement by {step}</Button>
-      </Div>
-    );
-  }
+  return (
+    <div>
+      <h1>Feedback Counter</h1>
+      <button onClick={() => handleFeedback('good')}>Good</button>
+      <button onClick={() => handleFeedback('neutral')}>Neutral</button>
+      <button onClick={() => handleFeedback('bad')}>Bad</button>
+      <div>
+        <h2>Feedback Statistics</h2>
+        <p>Good: {state.good}</p>
+        <p>Neutral: {state.neutral}</p>
+        <p>Bad: {state.bad}</p>
+      </div>
+    </div>
+  );
 }
+
 export default Counter;
